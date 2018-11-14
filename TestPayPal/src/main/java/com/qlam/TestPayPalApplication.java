6d3dd4a4c61bbbe4d7ed7d45fbd86939c0ab9cea
NTAnhDinh <15130033@st.hcmuaf.edu.vn>
2018-11-14 21:57:04 +0700

    Three commit

---
+++ TestPayPal/src/main/java/com/qlam/TestPayPalApplication.java
@@ -0,0 +1,12 @@
+package com.qlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPayPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPayPalApplication.class, args);
+	}
+}
